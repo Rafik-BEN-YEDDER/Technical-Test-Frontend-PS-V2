edd68ce716390d85f925117ee39e46af9b20148b
Rafik BEN YEDDER <benyedderrafik@gmail.com>
2022-10-27 17:22:40 +0100

    first commit

---
+++ build/precache-manifest.09730b22aeaa2f91591ef0c6ae1a16cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9f858b140802ef4b753524033381fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7311f769616c9901345",
+    "url": "/static/css/2.73669cad.chunk.css"
+  },
+  {
+    "revision": "e1addcc3bbd944bec9c9",
+    "url": "/static/css/main.4f4555ee.chunk.css"
+  },
+  {
+    "revision": "b7311f769616c9901345",
+    "url": "/static/js/2.d3a1e4cc.chunk.js"
+  },
+  {
+    "revision": "e1addcc3bbd944bec9c9",
+    "url": "/static/js/main.0735be28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
